d538615338ff1f2ce26c6594f799916ce2bc3f8e
Yuxing Fei <yuxingfei@berkeley.edu>
2024-02-27 01:35:31 -0800

    peak merging algorithm

---
+++ src/dara/peak_detection.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+import pandas as pd
+
+from dara.eflech_worker import EflechWorker, merge_peaks
+
+
+def detect_peak(
+    pattern: Union[Path, np.ndarray],
+    instrument_name: str = "Aeris-fds-Pixcel1d-Medipix3",
+    wmin: float = None,
+    wmax: float = None,
+) -> pd.DataFrame:
+    eflech_worker = EflechWorker()
+    peaks = eflech_worker.run_peak_detection(
+        pattern=pattern, instrument_name=instrument_name, wmin=wmin, wmax=wmax
+    )
+    peaks = merge_peaks(peaks, resolution=0.1)
+
+    return peaks
